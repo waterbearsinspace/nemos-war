c6d571e010bb004ec5d214c5d89dd62024811929
nicolasviste <nicolasviste@gmail.com>
2025-03-08 12:44:53 -0800

    Add A and J ship groups

---
+++ src/common/scripts/ships.ts
@@ -0,0 +1,4 @@
+// 12 gange: +1 treasure
+// 18 pequod: +1 nemo
+// 23 syria: +1 crew
+// 27 triumph: +1 treasure
--- src/common/stores/slices/shipPoolsSlice.ts
+++ src/common/stores/slices/shipPoolsSlice.ts
@@ -1,2 +1,41 @@
 // free ships
 // ships in ship pool
+
+// groups
+// white: A - non-warships
+// gray: J - early warships on back of non-warships
+// lightYellow: B - early warships
+// blue: D - notoriety warships
+// green: E - notoriety warships
+// darkYellow - C - modern warships
+// orange - F - modern warships
+// red - G - modern warships
+// black - H - special event warships
+// purple - J - up-gunned warships
+
+// const shipGroups = Object.freeze({
+//   A: "white",
+//   J: "gray,",
+// });
+
+// types and interfaces
+import { vp } from "../../scripts/calculateVictoryPoints";
+type ship = {
+  id: number;
+  name: string;
+  attackStrength: number | null;
+  defenseStrength: number;
+  notoriety: number;
+  groupId: string;
+  flipGroupId: string;
+  nationality: string;
+  shipClass: string;
+  vp: vp;
+  flipName: string;
+  flipAttackStrength: number | null;
+  flipDefenseStrength: number;
+  flipNotoriety: number;
+  flipNationality: string;
+  flipShipClass: string;
+  flipVp: vp;
+};
