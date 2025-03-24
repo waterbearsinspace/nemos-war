edcfcd9b39597d2694ebce0969d185c292d5a453
nicolasviste <nicolasviste@gmail.com>
2025-03-23 20:30:20 -0700

    Add plan for round structure

--- src/components/Screens/Playing/Playing.tsx
+++ src/components/Screens/Playing/Playing.tsx
@@ -1,10 +1,38 @@
-// game store
+//  game store
 import { nemosStore } from "../../../common/stores/nemosStore";
 
 export default function Playing() {
-  return (
-    <>
-      <p>test</p>
-    </>
-  );
+  //  # event phase
+  //  draw card
+  //  resolveCard
+
+  //  # placement phase
+  //  roll current act dice
+  //  get differential from white dice for AP and placement
+  //  if 0, lull turn
+  //  for each die, place ship
+  //    ignore black if lull turn
+
+  //  # lull turn
+  //  place treasure in doubles ocean and on top of adventure deck
+  //  crush uprisings
+  //
+  //  # action phase
+  //  adventure
+  //  attack
+  //  incite
+  //  move
+  //  rest
+  //  repair
+  //  refit
+  //  search
+  //
+  //  # check game end conditions
+  //  ship resource depleted
+  //  notoriety reaches limit
+  //  every ocean is full
+  //  reach finale card
+  //
+
+  return <></>;
 }
