9139703c3e533e6b2e7a80af533767bffb4a024b
nicolasviste <nicolasviste@gmail.com>
2025-04-16 17:14:31 -0700

    Properly remove debug from setup done screen

--- src/components/Screens/Setup/Screens/Done.tsx
+++ src/components/Screens/Setup/Screens/Done.tsx
@@ -7,64 +7,64 @@ import {
   getSubPhaseNumber,
 } from "../../../../common/utils/utils";
 
-function DrawPileList() {
-  const drawPile = nemosStore((state) => state.drawPile);
+// function DrawPileList() {
+//   const drawPile = nemosStore((state) => state.drawPile);
 
-  return (
-    <div>
-      <h2>Draw Pile</h2>
-      <div className="list-wrapper">
-        <ul className="card-list">
-          {drawPile.map((card) => {
-            return (
-              <li className={card.id > 1000 ? "bold" : ""} key={card.id}>
-                {card.title}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-}
-function AdventureDeck() {
-  const adventureDeck = nemosStore((state) => state.adventureDeck);
+//   return (
+//     <div>
+//       <h2>Draw Pile</h2>
+//       <div className="list-wrapper">
+//         <ul className="card-list">
+//           {drawPile.map((card) => {
+//             return (
+//               <li className={card.id > 1000 ? "bold" : ""} key={card.id}>
+//                 {card.title}
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+// function AdventureDeck() {
+//   const adventureDeck = nemosStore((state) => state.adventureDeck);
 
-  return (
-    <div>
-      <h2>Adventure Deck</h2>
-      <div className="list-wrapper">
-        <ul className="card-list">
-          {adventureDeck.map((card) => {
-            return <li key={card.id}>{card.title}</li>;
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-}
-function ShipPool() {
-  const shipPool = nemosStore((state) => state.currentShipPool);
+//   return (
+//     <div>
+//       <h2>Adventure Deck</h2>
+//       <div className="list-wrapper">
+//         <ul className="card-list">
+//           {adventureDeck.map((card) => {
+//             return <li key={card.id}>{card.title}</li>;
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+// function ShipPool() {
+//   const shipPool = nemosStore((state) => state.currentShipPool);
 
-  return (
-    <div>
-      <h2>Ship Pool</h2>
-      <div className="list-wrapper">
-        <ul className="card-list">
-          {shipPool.map((ship) => {
-            return (
-              <li key={typeof ship != "string" ? ship.id : 1}>
-                {typeof ship != "string"
-                  ? ship.name + ": " + ship.nationality + " " + ship.shipClass
-                  : "Hidden Ship"}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Ship Pool</h2>
+//       <div className="list-wrapper">
+//         <ul className="card-list">
+//           {shipPool.map((ship) => {
+//             return (
+//               <li key={typeof ship != "string" ? ship.id : 1}>
+//                 {typeof ship != "string"
+//                   ? ship.name + ": " + ship.nationality + " " + ship.shipClass
+//                   : "Hidden Ship"}
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default function Done() {
   let motive = nemosStore((state) => state.motiveName);
